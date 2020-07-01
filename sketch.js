b01a451dd8f144e006f08802c0a5483ed49cf796
logeshkarthik <55538801+logeshkarthik@users.noreply.github.com>
2020-07-01 19:17:05 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,36 @@
+let video;
+let poseNet;
+let pose;
+
+
+
+function setup() {
+  createCanvas(400, 400);
+  video =createCapture(VIDEO);
+  video.hide();
+  poseNet =ml5.poseNet(video,modelLoaded);
+  poseNet.on('pose',gotPoses);
+}
+function gotPoses(poses){
+console.log(poses);
+  if(poses.length>0){
+    pose=poses[0].pose;
+  }
+}
+function modelLoaded(){
+  console.log('posenet ready ');
+
+}
+
+function draw() {
+  image(video,0,0);
+  if(pose){
+    
+  fill(255,0,0);
+  ellipse(pose.nose.x, pose.nose.y, 64);
+    fill(0,0,255);
+    ellipse(pose.rightWrist.x,pose.rightWrist.y,32);
+    ellipse(pose.leftWrist.x,pose.leftWrist.y,32);
+  }
+  
+}
